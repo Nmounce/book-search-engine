8800d00b28a7f691c32c6d5c8776f8eff0fd1332
Nikki Mounce <nikkimounce@gmail.com>
2021-11-18 20:12:43 -0800

    added queries.js to public/utils

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
-// const routes = require('./routes');
-
 const { ApolloServer } = require('apollo-server-express');
 
-const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs, resolvers } = require('./schemas');
+
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 
 async function apolloServer() {
 //new instance of apollo server with GraphQL schema 
