facb46a1e3cb962b5834deb174f9947ebc22c666
Nikki Mounce <nikkimounce@gmail.com>
2021-11-20 14:14:47 -0800

    m

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 import { SAVE_BOOK } from '../utils/mutations';
@@ -71,7 +71,7 @@ const SearchBooks = () => {
 
     try {
       // const response = await saveBook(bookToSave, token);
-      const { data } = await savedBook({
+      const { data } = await saveBook({
         variables: { input: bookToSave }
       });
 
